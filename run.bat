0721b9568b22d59a0b83aaf93387525eae520239
silhavyj <jakub.sil@seznam.cz>
2022-09-26 16:25:13 +0200

    Formatting

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ DEL /S /Q "build"
 cmake -S . -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release && mingw32-make -C build
 
 if %ERRORLEVEL% == 0 goto :next
-	echo "ERROR: Failed to compile and build the application"
+    echo "ERROR: Failed to compile and build the application"
 
 :next
-	build\src\kivvss01.exe %1 %2 %3
+    build\src\kivvss01.exe %1 %2 %3
